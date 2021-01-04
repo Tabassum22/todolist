7188e4a9373d2a518405d8efbfd6410dbb10cc5c
Tabassum22 <syedtabassum224@gmail.com>
2021-01-04 13:27:04 +0530

    Todolist

---
+++ todo.js
@@ -0,0 +1,53 @@
+const todoForm = document.getElementById("todoForm");
+const todoList = document.getElementById("todoList");
+
+
+const todoListItems = [
+  { todo: "Milk", isChecked: false },
+  { todo: "Eggs", isChecked: false },
+  { todo: "Bread", isChecked: false },
+  { todo: "Veggies", isChecked: false },
+
+
+]
+
+
+todoForm.onsubmit = function (event) {
+  event.preventDefault();
+
+  const data = new FormData(todoForm);
+  let item = {
+    todo: data.get("todo"),
+    isChecked: false,
+  };
+  todoListItems.push(item);
+  todoList.innerHTML = "";
+  initTodo();
+  todoForm.reset();
+};
+
+
+function initTodo (){
+  for (let i = 0; i < todoListItems.length; i++) {
+    let newLi = document.createElement("li");
+
+    newLi.innerHTML =
+    `<div class="todo-list-item">
+    <input type="checkbox" name="milk" ${todoListItems[i].isChecked  "checked" : ""} id="todo_${
+      todoListItems[i].todo}" />
+      <span>${todoListItems[i].todo} </span></div>`;
+      todoList.appendChild(newLi);
+      document
+      .getElementById(`todo_${todoListItems[i].todo}`)
+      .addEventListener("click",function (e) {
+        let selectedItem = document.getElementById(e.target.id);
+        if (selectedItem.checked) {
+          selectedItem.nextElementsSibling.classList.add("strike-off");
+        } else {
+          selectedItem.nextElementsSibling.classList.remove("strike-off")''
+        }
+        
+      });
+  }
+}
+initTodo();
